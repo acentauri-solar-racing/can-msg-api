e2436515ee2cf904f44491a8f57e40268c6a0b63
tenderribs <dc3dude@gmail.com>
2023-02-20 16:18:07 +0100

    add validation script

---
+++ msg_data_types.py
@@ -0,0 +1,30 @@
+type_lookup = {
+    'data_fp':  {
+        'size': 32,
+        'c_type': 'float',
+    },
+    'data_u8': {
+        'size': 8,
+        'c_type': 'unsigned char',
+    },
+    'data_8': {
+        'size': 8,
+        'c_type': 'char',
+    },
+    'data_u16': {
+        'size': 16,
+        'c_type': 'unsigned int',
+    },
+    'data_16': {
+        'size': 16,
+        'c_type': 'int',
+    },
+    'data_u32': {
+        'size': 32,
+        'c_type': 'unsigned long',
+    },
+    'data_32': {
+        'size': 32,
+        'c_type': 'long',
+    }
+}
---
+++ validate_tree.py
@@ -0,0 +1,68 @@
+"""
+validate the tree, check if fields are set, indices and data types are aligned
+and do not exceed CAN message size.
+"""
+
+import yaml
+import os
+
+from pathlib import Path
+
+from msg_data_types import type_lookup
+
+# set the current working directory of script to resolve relative file path
+script_cwd: str = os.path.realpath(os.path.dirname(__file__))
+tree = yaml.safe_load(Path(script_cwd + '/msg-tree.yaml').read_text())
+
+print("Validating message tree")
+
+ids = []
+
+# move a cursor along 64 bits and check for overlaps or overflows
+def validate_fields(fields) -> bool:
+    cursor = 0
+    visited = [False] * 64 # array representing 64 bits, we mark bits as visited
+
+    for field in fields:
+        size = type_lookup[field['type']]['size']
+        idx = field['idx']
+
+        # check if the data will go out of bounds
+        cursor += size
+        if (cursor > 64):
+            print("topic " + topic + " exceeds 64 bits")
+            return False
+
+        # check if there is overlap between fields
+        start = size * idx
+        for i in range(start, start + size):
+            if visited[i] is True:
+                print("overlapping memory allocation in " + topic)
+                return False
+            else:
+                visited[i] = True
+
+    return True
+
+def validate_ids(ids) -> bool:
+    return True
+
+
+for namespace in tree:
+    for topic in tree[namespace]:
+        id = tree[namespace][topic]['id']
+        ids.append(id)
+
+        fields = []
+
+        for f in tree[namespace][topic]['data']:
+            fields.append(tree[namespace][topic]['data'][f])
+
+
+        if not validate_fields(fields):
+            print("data allocation error in topic: " + topic)
+
+    if not validate_ids(fields):
+        print("id error in topic, recheck: " + topic)
+
+print("Done")
