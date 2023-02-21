da336a2d68715630aa0f6114742484f8290a0a17
tenderribs <dc3dude@gmail.com>
2023-02-21 14:38:23 +0100

    follow changes to file and execute code line by line

--- can-monitor.py
+++ can-monitor.py
@@ -1,16 +1,14 @@
 """
 decode data from a log file and read the information therein
 """
-
+from multiprocessing import Lock
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
+import logging
 import time
 import os
 
 from pathlib import Path
-
-# set the current working directory of script to resolve relative file path
-script_cwd: str = os.path.realpath(os.path.dirname(__file__))
 
 
 class Watcher:
@@ -26,20 +24,45 @@ class Watcher:
         self.observer.start()
 
         print("CAN-Monitor running in {}\n".format(self.directory))
+
         try:
             while True:
                 time.sleep(1)
         except:
             self.observer.stop()
         self.observer.join()
-        print("\nWatcher Terminated\n")
+
+        print("CAN-Monitor gracefully terminated\n")
 
 
-class EventHandler(FileSystemEventHandler):
+class LogEventHandler(FileSystemEventHandler):
+    def __init__(self):
+        self.line_number = -1
+        self.lock = Lock()
+
     def on_modified(self, event):
-        print(event)
+        # fire only on modified files, not directories
+        if not event.is_directory:
+
+            # prevent concurrency issues with reading files
+            with self.lock:
+                with open(event.src_path, 'r', encoding="utf-8") as f:
+                    count = 0
+                    for line in f:
+                        if count > self.line_number:
+                            self._process_line(line)
+                            self.line_number = count
+
+                        count += 1
+
+    # Insert the values into MySql database
+    def _process_line(self, line):
+        print(line)
 
 
 if __name__ == "__main__":
-    w = Watcher(script_cwd + "/logs/", EventHandler())
+    # set the current working directory of script to resolve relative file path
+    script_cwd: str = os.path.realpath(os.path.dirname(__file__))
+
+    w = Watcher(script_cwd + "/logs/", LogEventHandler())
     w.run()
