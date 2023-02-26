c3cbe0fee51d78c62e2ff46fb5050704f5016011
tenderribs <dc3dude@gmail.com>
2023-02-26 16:38:16 +0100

    write models

--- source-tree.py
+++ source-tree.py
@@ -3,9 +3,10 @@ validate the tree, check if fields are set, indices and data types are aligned
 and do not exceed CAN message size.
 """
 
-import yaml
 import os
-import jinja2
+
+from yaml import safe_load
+from jinja2 import Environment, FileSystemLoader
 
 from pathlib import Path
 from typing import List
@@ -16,12 +17,13 @@ script_cwd: str = os.path.realpath(os.path.dirname(__file__))
 
 # flatten the tree into lists of topic and field dicts
 def flatten_tree() -> dict:
-    tree = yaml.safe_load(Path(script_cwd + "/msg-tree.yaml").read_text())
+    tree = safe_load(Path(script_cwd + "/msg-tree.yaml").read_text())
     topics: List = []
     ids: List = []
 
     for namespace in tree:
         for topic in tree[namespace]:
+            tree[namespace][topic]["name"] = topic
             ids.append(str(tree[namespace][topic]["id"]))
 
             topics.append(tree[namespace][topic])
@@ -82,6 +84,10 @@ def validate_tree() -> bool:
 
 
 def write_tree_to_fs():
+    def conv_name_camel_case(name: str):
+        words = [word.capitalize() for word in name.split(sep="_")]
+        return "".join(words)
+
     def conv_fields_to_type_index(can_id: str, fields: List, lines: List[str]) -> None:
         """autogenerate file for python-can with list of fields and types in type_lookup.txt"""
         cleaned_id = can_id.lstrip("0x")
@@ -91,15 +97,30 @@ def write_tree_to_fs():
 
         lines.append(cleaned_id + ":<" + "".join(struct_types) + "\n")
 
-    print("lmao")
+        with open(
+            script_cwd + "/type_lookup.txt", mode="w", encoding="utf-8"
+        ) as message:
+            message.writelines(lines)
+
+    # Use Jinja to write models to file using template
+    environment = Environment(loader=FileSystemLoader("templates/"))
+    template = environment.get_template("models.py.j2")
+
+    ids, topics = flatten_tree()
+    content = template.render(
+        conv_name_camel_case=conv_name_camel_case,
+        topics=topics,
+        type_lookup=type_lookup,
+    )
+
+    with open(script_cwd + "/db/models.py", mode="w", encoding="utf-8") as results:
+        results.write(content)
 
 
 if __name__ == "__main__":
-    print("Validating message tree")
-
     if validate_tree():
-        print("Done Validating, valid tree")
-
-        # write_tree_to_fs()
+        write_tree_to_fs()
     else:
         print("Done, ERROR state, invalid tree")
+
+    print("Done, messaging tree sourced to file system")
