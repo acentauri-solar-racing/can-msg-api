0350f41f6849f36d13c2fee7eaede42fc4a5d1ac
tenderribs <dc3dude@gmail.com>
2023-03-25 11:18:57 +0100

    catch duplicates in topic names

--- utils/helpers.py
+++ utils/helpers.py
@@ -3,13 +3,40 @@ import os
 from utils.type_lookup import type_lookup
 from typing import List
 
-from yaml import safe_load
+import yaml
+from yaml.constructor import ConstructorError
 from pathlib import Path
 
 # flatten the tree into lists of topic and field dicts
 ## slightly confusing that it says -> dict and outputs lists
 def flatten_tree() -> tuple:
-    tree = safe_load(Path("msg-tree.yaml").read_text())
+    try:
+        from yaml import CLoader as Loader
+    except ImportError:
+        from yaml import Loader
+
+    def no_duplicates_constructor(loader, node, deep=False):
+        """Check for duplicate keys."""
+        mapping = {}
+        for key_node, value_node in node.value:
+            key = loader.construct_object(key_node, deep=deep)
+            value = loader.construct_object(value_node, deep=deep)
+            if key in mapping:
+                raise ConstructorError(
+                    "while constructing a mapping",
+                    node.start_mark,
+                    "found duplicate key (%s)" % key,
+                    key_node.start_mark,
+                )
+            mapping[key] = value
+
+        return loader.construct_mapping(node, deep)
+
+    yaml.add_constructor(
+        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, no_duplicates_constructor
+    )
+
+    tree = yaml.full_load(Path("msg-tree.yaml").read_text())
     ids: List = []
     topics_list: List = []
     topics_dict: dict = {}
