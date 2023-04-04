80360fd77441dda07ad4042c86faca513ccf1c36
tenderribs <dc3dude@gmail.com>
2023-04-04 12:35:36 +0200

    copy code over

--- frontend/pages/bms.py
+++ frontend/pages/bms.py
@@ -1,4 +1,29 @@
-from dash import html
-from dash import html
+import plotly.express as px
+import dash_bootstrap_components as dbc
+import time
+import pandas as pd
 
-content = html.H1("BMS")
+from typing import Tuple
+from dash import html, dcc
+
+from db.models import *
+from db.db_service import DbService
+from pandas import DataFrame
+from frontend.styles import H1, H2
+
+
+def disp_mppt(power_df: DataFrame, stat) -> html.Div:
+    return html.Div([])
+
+
+def content():
+    db_serv: DbService = DbService()
+
+    try:
+        return html.Div([
+            html.H1("BMS"),
+        ])
+    except:
+        print("Err: Couldn't load BMS Tables")
+
+        return html.Div()
