45a452ad8471ed2859af33bab75048e9a03e3031
Frederike Brockmeyer <brockmef@ethz.ch>
2023-04-27 10:55:04 +0200

    overview page design, kinda

--- frontend/pages/overview.py
+++ frontend/pages/overview.py
@@ -12,22 +12,47 @@ from collections import OrderedDict
 dash.register_page(__name__, path='/', title='Overview')
 
 
-data = [
-    {'module': 'vcu', 'status': "active", 'last activity': "active"},
-    {'module': 'icu', 'status': "inactive", 'last activity': "active"},
-    {'module': 'mppt', 'status': "active", 'last activity': "active"},
+module_data = [
+    {'module': 'vcu', 'status': "active", 'last activity': "no idea"},
+    {'module': 'icu', 'status': "inactive", 'last activity': "don't really care"},
+    {'module': 'mppt', 'status': "active", 'last activity': "stop asking"},
+]
+
+speed = 300000000 * 3.6  # km/h
+power = 3000
+soc = 110
+
+# main_info = [f"Speed: {speed} km/h",
+#              f"Power Consumption Motor: {power} W",
+#              f"SOC Battery: {soc} %"]
+
+main_data = [
+    {'Speed': f"{speed} km/h", 'Power Consumption of Motor': f"{power} W",
+        'SOC of Battery': f"{soc} %"},
 ]
 
 
 def layout():
     return html.Div([
         html.H1("Overview"),
-        html.H2("Speed"),
-        html.B("Speed and some graph"),
+        html.H2("Car Status"),
+        # html.Ul(children=[html.Li(i) for i in main_info], style={'backgroundColor': 'black',
+        #                                                          'list-style-type': 'square',
+        #                                                          'color': 'white',
+        #                                                          'font_size': '30px', }),
+        dash_table.DataTable(data=main_data,
+                             id='main_table',
+                             style_data={
+                                 'font_size': '25px',
+                                 'font_weight': 'heavy'
+                             },
+                             style_as_list_view=True,
+                             ),
 
         html.H2("Module Status"),
-        dash_table.DataTable(data=data,
+        dash_table.DataTable(data=module_data,
                              id='activity_table',
+                             style_as_list_view=True,
                              style_data_conditional=[
 
                                  {
@@ -39,5 +64,6 @@ def layout():
                                      'color': 'white'
                                  },
 
-                             ])
+                             ],
+                             )
     ])
